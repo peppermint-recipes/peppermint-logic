ce9aaa12c9f7c02f5c5adc82deac96785325ef7e
eriksson <github@der-klinger.de>
2021-02-19 23:06:35 +0100

    Add checking of electron envronment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import isElectron from './utils/isElectron';
 import * as parseIngredients from './ingredientParser';
 import * as recipeService from './recipeService';
 import * as shoppingListService from './shoppingListService';
 import * as weekService from './weekService';
 
+console.log(isElectron());
+
+console.log('test');
 export {
   parseIngredients,
   recipeService,
